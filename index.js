2a186f8bbf5db72f134e01e3297dbbb7ebe00ce3
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-02-02 08:36:00 -0300

    index service implementation

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 //npm init
 //npm install express
+import facts from './data/data_collection.js';
+import {searchFact, validateYearInput} from './service.js';
 
 const express = require('./express');
 const app = express();
 const port = 8080;
+
+app.get('/', (req, res) => {
+    let year = req.query.ano;
+    if(validateYearInput(year)){
+        res.status(200).json(searchFact(year, facts));
+    }
+    res.status(400).json({'Erro' : "Ano informado inválido"});
+});
 
 app.listen(port, () => {
     let date = new Date();
