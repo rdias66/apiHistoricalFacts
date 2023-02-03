12c44bf10c3ab1f14b2785e603101f3efd6e52ff
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-02-03 11:15:08 -0300

    cruds post n delete added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //npm init
 //npm install express
 import facts from './data_collection.js';
-import {searchFact, validateYearInput} from './service.js';
+import {searchFact, validateYearInput, findFactPos} from './service.js';
 
 const express = require('./express');
 const app = express();
@@ -13,6 +13,26 @@ app.get('/', (req, res) => {
         res.status(200).json(searchFact(year, facts));
     }
     res.status(400).json({'Erro' : "Ano informado inválido"});
+});
+
+app.post('/', (req, res) => {
+    const newFactYear = req.query.ano;
+    if(!validateYearInput(newFactYear)){
+        const newFact = {Ano: newFactYear , Fato: req.query.fato }  
+        facts.push(newFact);
+        res.status(200);
+    }
+    res.status(400).send("Fato ja existe no ano informado");
+
+});
+
+app.delete('/', (req, res) => {
+    const yearToDelete = req.query.ano;
+    if(validateYearInput(year)){
+        facts.splice(findFactPos(year), 1);
+        res.status(200).send("Fato do ano" + year + "deletado");
+    }
+    res.status(400).send("Ano informado invalido");
 });
 
 app.listen(port, () => {
